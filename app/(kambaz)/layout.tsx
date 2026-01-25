cdcd59bbf8e6e938342f392c229a8e75348f4913
kikiliu7 <161527266+kikiliu7@users.noreply.github.com>
2026-01-25 18:13:41 -0500

    vercel fix

--- app/(kambaz)/layout.tsx
+++ app/(kambaz)/layout.tsx
@@ -1,19 +1,13 @@
 import { ReactNode } from "react";
-import CourseNavigation from "./Navigation";
-export default async function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
- const { id } = await params;
+import KambazNavigation from "./Navigation";
+export default function KambazLayout({ children }: Readonly<{ children: ReactNode }>) {
  return (
-   <div id="wd-courses">
-     <h2>Courses {id}</h2>
-     <hr />
-     <table>
-       <tbody>
-         <tr>
-           <td valign="top" width="200"> <CourseNavigation /> </td>
-           <td valign="top" width="100%"> {children} </td>
-         </tr>
-       </tbody>
-     </table>
-   </div>
+   <table>
+     <tbody>
+       <tr>
+         <td valign="top" width="200">  <KambazNavigation /> </td>
+         <td valign="top" width="100%"> {children}           </td>
+       </tr>
+     </tbody>
+   </table>
 );}
